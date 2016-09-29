64ef28f248ff622ce5ca0fa95defcec7aa9b28e2
Alexis Christoforides <alexis@thenull.net>
2016-09-29 14:31:39 +0300

    Transfer the Mac SDK bockbuild profiles & resources inside the Mono repository.
    This includes all distributable packages,patches etc. Toolchain packages remain part of the bockbuild repository.
    
    The profiles were also renamed:
    'mono-mac-release' -> 'mac-sdk'
    'mono-mac-xamarin' -> 'mac-sdk-xamarin'

---
+++ bockbuild/mac-sdk/gtk-sharp.py
@@ -0,0 +1,14 @@
+class GtkSharp212ReleasePackage (Package):
+
+    def __init__(self):
+        Package.__init__(self, 'gtk-sharp',
+                         sources=['git://github.com/mono/gtk-sharp.git'],
+                         git_branch='gtk-sharp-2-12-branch',
+                         revision='4ac71507fffce997207215ac3f8b38471ce60a14',
+                         override_properties={
+                             'configure': './bootstrap-2.12 --prefix=%{package_prefix}',
+                         }
+                         )
+        self.make = 'make CSC=mcs'
+
+GtkSharp212ReleasePackage()
